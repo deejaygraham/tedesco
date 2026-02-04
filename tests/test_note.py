0a2ed16f6f9869599b637bebe7776bab261d0133
Derek Graham <deejaygraham@gmail.com>
2026-02-04 09:04:44 +0000

    Update test_note.py

--- tests/test_note.py
+++ tests/test_note.py
@@ -3,6 +3,16 @@ import pytest
 from tedesco.note import Note
 from tedesco.interval import Interval
 
+def test_negative_note_values_are_not_allowed():
+    with pytest.raises(TypeError):
+        _ = Note(-1)
+
+def test_note_distance_between_adjacent_notes_is_halfstep():
+    assert (Note(0, 4) - Note(1, 4)).semitones == 1
+
+def test_note_distance_between_white_notes_is_wholestep():
+    assert (Note(50, 4) - Note(52, 4)).semitones == 2
+
 def test_subtracting_one_note_from_another_returns_interval():
     c4 = Note(0, 4)   # C4
     e4 = Note(4, 4)   # E4 (4 semitones above C)
