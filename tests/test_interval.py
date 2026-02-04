e3d1d9983e18d2e0f8788b7645e1d799d8d39446
Derek Graham <deejaygraham@gmail.com>
2026-02-04 08:31:52 +0000

    Update test_interval.py

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -6,43 +6,44 @@ import operator
 from tedesco.interval import Interval
 
 
-def test_interval_construction_and_value():
+def test_positive_intervals_are_valid():
     i = Interval(7)
     assert i.semitones == 7
 
+def test_negative_intervals_are_valid():
     j = Interval(-3)
     assert j.semitones == -3
 
+def test_zero_intervals_are_valid():
     k = Interval(0)
     assert k.semitones == 0
 
-
-def test_interval_is_frozen_immutable():
+def test_interval_cannot_be_modified_after_creation():
     i = Interval(5)
     with pytest.raises(FrozenInstanceError):
         i.semitones = 6  # type: ignore[attr-defined]
 
-
-def test_equality_and_hashing():
+def test_intervals_can_be_equal():
     a = Interval(7)
     b = Interval(7)
-    c = Interval(-7)
     assert a == b
-    assert a != c
 
-    # hashable & set behavior
-    s = {a, b, c}
-    assert len(s) == 2  # a and b collapse; c is distinct
+def test_intervals_can_be_unequal():
+    a = Interval(7)
+    b = Interval(6)
+    assert a != b
 
-
-def test_repr_format_positive_negative_zero():
+def test_interval_repr_format_positive():
     assert re.fullmatch(r"<Interval \+7 st>", repr(Interval(7)))
+
+def test_interval_repr_format_negative():
     assert re.fullmatch(r"<Interval -3 st>", repr(Interval(-3)))
+
+def test_interval_repr_format_zero():
     # zero uses + sign per implementation
     assert re.fullmatch(r"<Interval \+0 st>", repr(Interval(0)))
 
-
-def test_scalar_multiplication_left_and_right():
+def test_interval_can_be_multiplied_by_integer():
     i = Interval(3)
     assert (i * 2).semitones == 6
     assert (2 * i).semitones == 6
@@ -55,16 +56,16 @@ def test_scalar_multiplication_left_and_right():
     assert (i * 1).semitones == 3
     assert (i * 0).semitones == 0
 
-
-def test_scalar_multiplication_type_errors():
+def test_interval_multiplied_by_float_throws_error():
     i = Interval(5)
     # Unsupported scalar types should return NotImplemented → TypeError
     with pytest.raises(TypeError):
         _ = i * 2.5  # float not supported
 
+def test_interval_multiplied_by_string_throws_error():
+    i = Interval(5)
     with pytest.raises(TypeError):
         _ = "x" * i  # __rmul__ only supports int, not str-left
-
 
 def test_add_with_unsupported_type_raises_typeerror():
     i = Interval(2)
@@ -76,19 +77,9 @@ def test_add_with_unsupported_type_raises_typeerror():
     with pytest.raises(TypeError):
         _ = i + object()
 
-
-def test_notimplemented_protocol_for_float():
-    i = Interval(5)
-    assert Interval.__mul__(i, 2.5) is NotImplemented
-    # Using operator.mul triggers Python’s numeric protocol -> TypeError
-    with pytest.raises(TypeError):
-        operator.mul(i, 2.5)
-
-
 def test_interval_less_than_comparison():
     assert Interval.PerfectFifth < Interval.Octave
 
-
-def test_interval_maths_equals_static():
+def test_interval_well_known_multiplication():
     assert Interval.MajorThird * 2 == Interval.MajorSixth
     assert 2 * Interval.Fifth == Interval(14)
