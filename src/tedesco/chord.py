095e0057825341350a46164a7babbd2f65bc092f
Derek Graham <deejaygraham@gmail.com>
2026-02-06 10:18:19 +0000

    Update chord.py

--- src/tedesco/chord.py
+++ src/tedesco/chord.py
@@ -90,7 +90,8 @@ class Chord:
             pattern = value
           
         degrees = _parse_csv(pattern)
-        self.intervals = [Interval(n) for n in degrees]
+        # For frozen dataclasses we must use object.__setattr__ in __init__
+        object.__setattr__(self, "intervals", [Interval(n) for n in degrees])
       
     # ---- Basic container protocol ------------------------------------------
     def __iter__(self) -> Iterator[Interval]:
