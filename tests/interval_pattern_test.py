3c64cdb1500f2ab8043e88d5e83fe9661aad6be8
Derek Graham <deejaygraham@gmail.com>
2026-02-06 13:57:41 +0000

    Update interval_pattern_test.py

--- tests/interval_pattern_test.py
+++ tests/interval_pattern_test.py
@@ -11,11 +11,13 @@ def test_pattern_must_not_be_blank():
       
 def test_pattern_from_single_interval():
     i = IntervalPattern("3")
-    assert len(i.intervals) == 2
+    assert len(i.intervals) == 1
 
 def test_pattern_ignores_whitespace():
     j = IntervalPattern(" 2, 1  ")
-    assert len(j) == 3
+    assert len(j) == 2
 
-def test_pattern_contains_correct_intervals():
-  pass
+def test_pattern_converts_to_notes():
+    pattern = IntervalPattern("2, 1")
+    root = Note("C4")
+    assert len(pattern.to_notes(root)) == 3
