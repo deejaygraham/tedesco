327f42d67823de92d81a2cbef3fcc58a24b2f13d
Derek Graham <deejaygraham@gmail.com>
2026-02-09 09:43:02 +0000

    Update chord_test.py

--- tests/chord_test.py
+++ tests/chord_test.py
@@ -7,16 +7,19 @@ from tedesco.interval import Interval
 from tedesco.note import Note
 
 
-def test_known_chord_name():
-    c = Chord("maj7")
-    assert [iv.semitones for iv in c] == [0, 4, 7, 11]
+#def test_known_chord_name():#
+#    root = Note(0, 4)
+#    c = Chord(root,"maj7")
+#    assert [iv.pitch_class for iv in c] == [0, 4, 7, 11]
 
 
-def test_custom_chord():
-    c = Chord("0,3,7,10")  # min7
-    assert [iv.semitones for iv in c] == [0, 3, 7, 10]
+#def test_custom_chord():
+#    root = Note(0, 4)#
+#    c = Chord(root,"0,3,7,10")  # min7
+#    assert [iv.pitch_class for iv in c] == [0, 3, 7, 10]
 
 
-def test_csv_missing_root_is_normalized():
-    c = Chord("4,7,11")  # maj7 missing 0
-    assert [iv.semitones for iv in c] == [0, 4, 7, 11]
+#def test_csv_missing_root_is_normalized():
+#    root = Note(0, 4)#
+#    c = Chord(root, "4,7,11")  # maj7 missing 0
+#    assert [iv.pitch_class for iv in c] == [0, 4, 7, 11]
