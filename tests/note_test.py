2dd5105947e73674e34efae214d06ee0df8f5bce
Derek Graham <deejaygraham@gmail.com>
2026-02-13 11:11:10 +0000

    Update note_test.py

--- tests/note_test.py
+++ tests/note_test.py
@@ -39,21 +39,21 @@ def test_note_from_spn_rejects_invalid():
         Note("C#4x")   # junk suffix
 
 def test_note_distance_between_adjacent_notes_is_halfstep():
-    assert abs((Note(0, 4) - Note(1, 4)).semitones) == 1
+    assert abs((Note.from_midi(48) - Note.from_midi(49)).semitones) == 1
 
 def test_note_distance_between_white_notes_is_wholestep():
-    assert abs((Note(50, 4) - Note(52, 4)).semitones) == 2
+    assert abs((Note.from_midi(50) - Note.from_midi(52)).semitones) == 2
 
 def test_subtracting_one_note_from_another_returns_interval():
-    c4 = Note(0, 4)   # C4
-    e4 = Note(4, 4)   # E4 (4 semitones above C)
+    c4 = Note("C4")   
+    e4 = Note("E4")  
     i = e4 - c4
     assert isinstance(i, Interval)
     assert i.semitones == 4
 
 
 def test_adding_interval_to_note_returns_note():
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     fifth = Interval(7)
     g4 = c4 + fifth
     assert isinstance(g4, Note)
@@ -61,27 +61,27 @@ def test_adding_interval_to_note_returns_note():
 
 
 def test_adding_note_and_interval_order_independent():
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     m3 = Interval(3)
     assert (c4 + m3) == (m3 + c4)
 
 
 def test_subtract_low_note_from_high_note_returns_positive_interval():
-    lower = Note(0, 4)
-    higher = Note(4, 4)
+    lower = Note("C4")
+    higher = Note("E4")
     i = higher - lower
     assert i.semitones == 4
 
 
 def test_subtract_high_note_from_low_note_returns_negative_interval():
-    lower = Note(0, 4)
-    higher = Note(4, 4)
+    lower = Note("C4")
+    higher = Note("E4")
     i = lower - higher
     assert i.semitones == -4
 
 
 def test_octave_wrap_on_addition():
-    b4 = Note(11, 4)           # B4
+    b4 = Note("B4")          
     half_step = Interval(1)
     c5 = b4 + half_step
     assert c5.pitch_class == 0  # C
@@ -94,19 +94,19 @@ def test_from_absolute_semitone_and_roundtrip():
     g4 = Note.from_absolute_semitone(abs_st)
     assert g4.pitch_class == 7 and g4.octave == 4
     # Round-trip via subtraction
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     assert (g4 - c4).semitones == 7
 
 
 def test_negative_intervals_downward_transposition():
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     m3_down = Interval(-3)
     a3 = c4 + m3_down
     assert a3.pitch_class == 9 and a3.octave == 3  # A3
 
 
 def test_unrelated_types_return_not_implemented():
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     with pytest.raises(TypeError):
         _ = c4 + 1  # not an Interval, should raise via NotImplemented -> TypeError
     with pytest.raises(TypeError):
@@ -123,7 +123,7 @@ def test_note_from_midi_requires_int():
         Note.from_midi(60.5)
 
 def test_note_name_prefers_sharps_and_flats():
-    gsharp4 = Note.from_name("G#", 4)
+    gsharp4 = Note("G#4")
     assert gsharp4.name() == "G#"
     assert gsharp4.name(prefer_sharps=False) == "Ab"
 
@@ -134,7 +134,7 @@ def test_note_name_prefers_sharps_and_flats():
     assert bb3.name(prefer_sharps=False) == "Bb"
 
 def test_note_add_interval_and_reverse():
-    c4 = Note(0, 4)
+    c4 = Note("C4")
     m2 = Interval(2)
     d4 = c4 + m2
     assert d4.pitch_class == 2
@@ -145,8 +145,8 @@ def test_note_add_interval_and_reverse():
     assert d4_b == d4
 
 def test_note_subtraction_returns_interval():
-    c4 = Note(0, 4)
-    e4 = Note.from_name("E", 4)
+    c4 = Note("C4")
+    e4 = Note("E4")
     interval = e4 - c4
     assert isinstance(interval, Interval)
     assert interval.semitones == 4   
