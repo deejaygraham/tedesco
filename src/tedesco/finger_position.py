1fbaa45bc734b7cd0d3cd9f62dcfe253df8d6189
Derek Graham <deejaygraham@gmail.com>
2026-02-16 13:22:57 +0000

    Update finger_position.py

--- src/tedesco/finger_position.py
+++ src/tedesco/finger_position.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
 class FingerPosition:
+    """Simple model of one position on a guitar fingerboard"""
   
     def __init__(self, string: int, fret: int):
+        """Initializes the position with a string and a fret.
+        String is the number of the string (1 is the highest pitch string).
+        Fret is the number of the fret from the nut (0 is the nut).
+        Args:
+            string (int): The string number (1-6)
+            fret (int): The fret number (0-24)
+        """
         if string < 1 or string > 6:
             raise ValueError("String must be between 1 and 6")
         if fret < 0 or fret > 24:
