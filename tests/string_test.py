95802a311491d4d226c858995534850c6d7c722d
Derek Graham <deejaygraham@gmail.com>
2026-02-16 12:25:44 +0000

    Update string_test.py

--- tests/string_test.py
+++ tests/string_test.py
@@ -3,5 +3,5 @@ from tedesco.string import String
 import pytest
 
 def test_string_created_with_string_and_note():
-    high_e = String(Note(1, "E5"))
+    high_e = String(1, Note("E5"))
                     
