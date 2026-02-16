c1b72f66a4eae263d63eeedb87736fd07a3eb119
Derek Graham <deejaygraham@gmail.com>
2026-02-16 11:01:28 +0000

    Update note_test.py

--- tests/note_test.py
+++ tests/note_test.py
@@ -5,20 +5,20 @@ from tedesco.interval import Interval
 
 def test_negative_note_values_are_not_allowed():
     with pytest.raises(TypeError):
-        _ = Note(-1)
+        Note(-1)
 
 def test_note_from_name_valid():
     c4 = Note("C4")
-    assert c4.name == "C"
+    assert c4.pitch == "C"
     assert c4.octave == 4
 
-def test_note_ignores_whitespace_name():
-    fs3 = Note("  F#  3")
-    assert fs3.pitch == "F#"
-
+def test_note_name_with_whitespace_throws():
+    with pytest.raises(ValueError):
+        Note("  F#  3")
+    
 def test_note_from_unknown_name_throws():
     with pytest.raises(ValueError):
-        Note.from_name("H", 4)
+        Note("H4")
 
 def test_note_from_spn_parses_valid_string():
     c4 = Note("C4")
@@ -27,7 +27,7 @@ def test_note_from_spn_parses_valid_string():
 
 def test_note_from_spn_parses_whitespace_string():
     eb5 = Note(" Eb5 ")
-    assert eb5.pitch_class == 3
+    assert eb5.pitch == "D#"
     assert eb5.octave == 5
 
 def test_note_from_spn_rejects_invalid():
@@ -57,7 +57,7 @@ def test_adding_interval_to_note_returns_note():
     fifth = Interval(7)
     g4 = c4 + fifth
     assert isinstance(g4, Note)
-    assert g4.pitch_class == 7 and g4.octave == 4  # G4
+    assert g4.pitch == 'G' and g4.octave == 4  
 
 
 def test_adding_note_and_interval_order_independent():
@@ -84,18 +84,8 @@ def test_octave_wrap_on_addition():
     b4 = Note("B4")          
     half_step = Interval(1)
     c5 = b4 + half_step
-    assert c5.pitch_class == 0  # C
+    assert c5.pitch == 'C'
     assert c5.octave == 5
-
-
-def test_from_absolute_semitone_and_roundtrip():
-    # absolute semitone is pc + 12*octave with C0 == 0
-    abs_st = 12 * 4 + 7  # G4
-    g4 = Note.from_absolute_semitone(abs_st)
-    assert g4.pitch_class == 7 and g4.octave == 4
-    # Round-trip via subtraction
-    c4 = Note("C4")
-    assert (g4 - c4).semitones == 7
 
 
 def test_negative_intervals_downward_transposition():
@@ -116,28 +106,21 @@ def test_note_from_midi_and_to_midi_roundtrip():
     c4 = Note.from_midi(60)       # middle C
     assert c4.pitch == "C"
     assert c4.octave == 4
-    assert c4.to_midi() == 60
+    assert c4.midi == 60
 
 def test_note_from_midi_requires_int():
     with pytest.raises(TypeError):
         Note.from_midi(60.5)
 
-def test_note_name_prefers_sharps_and_flats():
-    gsharp4 = Note("G#4")
-    assert gsharp4.name() == "G#"
-    assert gsharp4.name(prefer_sharps=False) == "Ab"
-
-    bb3 = Note("Bb3")
-    # prefer_sharps will show A#
-    assert bb3.name() == "A#"
-    # flat-oriented path returns Bb
-    assert bb3.name(prefer_sharps=False) == "Bb"
+def test_note_name_prefers_sharps():
+    assert Note("G#4").pitch == "G#"
+    assert Note("Bb3").pitch == "A#"
 
 def test_note_add_interval_and_reverse():
     c4 = Note("C4")
     m2 = Interval(2)
     d4 = c4 + m2
-    assert d4.pitch_class == 2
+    assert d4.pitch == "D"
     assert d4.octave == 4
 
     # Interval + Note via __radd__
