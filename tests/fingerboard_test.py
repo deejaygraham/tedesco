f9bc053d51036716b13cae69d6e748513e0a37eb
Derek Graham <deejaygraham@gmail.com>
2026-02-16 14:25:10 +0000

    Update fingerboard_test.py

--- tests/fingerboard_test.py
+++ tests/fingerboard_test.py
@@ -5,11 +5,11 @@ import pytest
 
 def test_fingerboard_standard_tuning():
     fb = Fingerboard()
-    assert fb.note_at(1, 0) == Note("E5")
-    assert fb.note_at(6, 0) == Note("E3")
+    assert fb.at(1, 0) == Note("E5")
+    assert fb.at(6, 0) == Note("E3")
 
 def test_fingerboard_notes_at_higher_frets():
     fb = Fingerboard()
-    assert fb.note_at(1, 12) == Note("E6")
-    assert fb.note_at(3, 5) == Note("F4")
+    assert fb.at(1, 12) == Note("E6")
+    assert fb.at(3, 5) == Note("F4")
   
