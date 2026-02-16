5150c4f506ee551f1099dba14def90bfa807d69c
Derek Graham <deejaygraham@gmail.com>
2026-02-16 12:19:41 +0000

    Create string_test.py

---
+++ tests/string_test.py
@@ -0,0 +1,7 @@
+from tedesco.note import Note
+from tedesco.string import String
+import pytest
+
+def test_string_created_with_string_and_note():
+    high_e = String(Note(1, "E5")
+                    
