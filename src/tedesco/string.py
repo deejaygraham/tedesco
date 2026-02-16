7678410140b52b0b2321544b877af40bc24bd308
Derek Graham <deejaygraham@gmail.com>
2026-02-16 12:45:16 +0000

    Update string.py

--- src/tedesco/string.py
+++ src/tedesco/string.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 import re
 from .note import Note
+from .interval import Interval
 
 class String:
     """Models a guitar string"""
+
+    __slots__ = ("_number", "_tuning")
 
     def __init__(self, string_number: int, tuning: Note):
         """Initializes the string with a number and a midi tuning"""
@@ -12,35 +15,35 @@ class String:
         if not isinstance(tuning, Note):
             raise ValueError(f"Tuning must be a valid note")
 
-        self.number = string_number
-        self.tuning = tuning
+        self._number = string_number
+        self._tuning = tuning
 
     def __str__(self):
         """Returns a string representation of the string"""
-        return f"String {self.number}"
+        return f"String {self._number}"
 
     def __eq__(self, other):
         """Checks if two strings are equal"""
-        return self.number == other.number and self.tuning == other.tuning
+        return self._number == other._number and self._tuning == other._tuning
 
     def __hash__(self):
         """Returns a hash of the string"""
-        return hash((self.number, self.tuning))
+        return hash((self._number, self._tuning))
 
     def tune_to(self, tuning: Note):
         """Tunes the string to a given tuning"""
-        if not isinstance(other, Note):
+        if not isinstance(tuning, Note):
             raise ValueError(f"Tuning must be a valid note")
             
-        self.tuning = tuning
+        self._tuning = tuning
 
     @property
     def tuning(self) -> Note:
-        return self.tuning
-
+        return self._tuning
+    
     def at_fret(self, fret: int) -> Note:
         """Returns the value of the string at a given fret"""
         if fret < 0 or fret > 24:
             raise ValueError("Fret number must be between 0 and 24")
             
-        return self.tuning + fret
+        return self.tuning + Interval(fret)
