e762a71ac3f5135ab6052e5e7080fce0f8b130fe
Derek Graham <deejaygraham@gmail.com>
2026-02-16 14:18:33 +0000

    Create fingerboard_test.py

---
+++ tests/fingerboard_test.py
@@ -0,0 +1,15 @@
+from tedesco.note import Note
+from tedesco.string import String
+from tedesco.fingerboard import Fingerboard
+import pytest
+
+def test_fingerboard_standard_tuning():
+    fb = Fingerboard()
+    assert fb.note_at(1, 0) == Note("E5")
+    assert fb.note_at(6, 0) == Note("E3")
+
+def test_fingerboard_notes_at_higher_frets():
+    fb = Fingerboard()
+    assert fb.note_at(1, 12) == Note("E6")
+    assert fb.note_at(3, 5) == Note("F4")
+  
